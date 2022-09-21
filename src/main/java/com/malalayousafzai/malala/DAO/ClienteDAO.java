8e2b00493d6e18a9314ec0874781de4924a9088b
camila <camila.queiroz@sensedia.com>
2022-09-21 09:46:53 -0300

    Insere dependencia do swagger

--- src/main/java/com/malalayousafzai/malala/DAO/ClienteDAO.java
+++ src/main/java/com/malalayousafzai/malala/DAO/ClienteDAO.java
@@ -3,7 +3,9 @@ package com.malalayousafzai.malala.DAO;
 
 import com.malalayousafzai.malala.model.Cliente;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ClienteDAO extends CrudRepository<Cliente, Integer> {
 
 }
